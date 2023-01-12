1923f4bbf67ba745c99f20f32715b81d0bf828f7
Edson Lima Costa <ecosta@alelo.com.br>
2023-01-12 01:04:00 -0300

    feat: versionando arquivos

---
+++ rest-spring-boot-java/src/test/java/br/com/edson/restspringboot/RestSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edson.restspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
