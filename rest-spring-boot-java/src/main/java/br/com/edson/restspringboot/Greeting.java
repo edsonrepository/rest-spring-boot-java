c54fe724d92574d9dcf4d4c9276ae74d14a1ca6e
Edson Lima Costa <ecosta@alelo.com.br>
2023-01-12 01:04:11 -0300

    feat: primeiro controller

---
+++ rest-spring-boot-java/src/main/java/br/com/edson/restspringboot/Greeting.java
@@ -0,0 +1,15 @@
+package br.com.edson.restspringboot;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+}
